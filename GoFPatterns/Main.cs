198df3ff550275748d2415ba8569b1ef55e9f672
Noon-R <noon.ryou0304@gmail.com>
2022-05-30 07:49:55 +0900

    add EntryPoint

---
+++ GoFPatterns/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoFPatterns
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
